b8e814f5da9c8e92f578fe2cb00077d6c6770f1a
FelipeFerraz4 <felipeferraz2402@gmail.com>
2025-08-06 15:17:27 -0300

    doc: update person class with database

--- src/main/java/space/algoritmos/habit_tracker_back/model/Person.java
+++ src/main/java/space/algoritmos/habit_tracker_back/model/Person.java
@@ -1,19 +1,49 @@
 package space.algoritmos.habit_tracker_back.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+/**
+ * The Person class represents a person with common user attributes.
+ */
+
+@Entity
+@Table(name = "people")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false, updatable = false)
+    private UUID id;
+
+    @Column(nullable = false)
     private String name;
+
+    @Column(unique = true,  nullable = false)
     private String email;
+
+    @Column(name = "phone_number")
     private String phoneNumber;
+
+    @Column(nullable = false)
     private String profilePictureUrl;
+
+    @Column(nullable = false)
     private String bio;
-    private String password;
+
+    @Column(nullable = false)
+    private String status;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
 }
