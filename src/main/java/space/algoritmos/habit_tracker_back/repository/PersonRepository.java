6d850b3a26c044317709de747dcf7db12026da82
FelipeFerraz4 <felipeferraz2402@gmail.com>
2025-08-07 16:48:23 -0300

    feat: create user service

--- src/main/java/space/algoritmos/habit_tracker_back/repository/PersonRepository.java
+++ src/main/java/space/algoritmos/habit_tracker_back/repository/PersonRepository.java
@@ -1,9 +1,11 @@
 package space.algoritmos.habit_tracker_back.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import space.algoritmos.habit_tracker_back.model.Person;
 
 import java.util.UUID;
 
+@Repository
 public interface PersonRepository extends JpaRepository<Person, UUID> {
 }
