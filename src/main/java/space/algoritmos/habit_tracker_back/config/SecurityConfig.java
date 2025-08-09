5cf4be1dea87afcc25c9bed9673718484a1fbd35
FelipeFerraz4 <felipeferraz2402@gmail.com>
2025-08-08 23:33:29 -0300

    feat: adding cors in security config

--- src/main/java/space/algoritmos/habit_tracker_back/config/SecurityConfig.java
+++ src/main/java/space/algoritmos/habit_tracker_back/config/SecurityConfig.java
@@ -79,4 +79,16 @@ public class SecurityConfig {
                 .build();
     }
 
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("http://localhost:3000");
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
 }
