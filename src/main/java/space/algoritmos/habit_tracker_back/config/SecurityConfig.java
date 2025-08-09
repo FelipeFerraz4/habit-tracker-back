21fc1068b969154a28c65a0293af73569a059522
FelipeFerraz4 <felipeferraz2402@gmail.com>
2025-08-08 23:27:16 -0300

    feat: create password encoder in security config

---
+++ src/main/java/space/algoritmos/habit_tracker_back/config/SecurityConfig.java
@@ -0,0 +1,54 @@
+package space.algoritmos.habit_tracker_back.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import space.algoritmos.habit_tracker_back.security.jwt.JwtTokenFilter;
+import space.algoritmos.habit_tracker_back.security.jwt.JwtTokenProvider;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    private final JwtTokenProvider tokenProvider;
+    private final String passwordSecret;
+
+    public SecurityConfig(JwtTokenProvider tokenProvider,
+                          @Value("${spring.security.password.secret}") String passwordSecret) {
+        this.tokenProvider = tokenProvider;
+        this.passwordSecret = passwordSecret;
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        PasswordEncoder pbkdf2Encoder = new Pbkdf2PasswordEncoder(
+                passwordSecret, 10, 185000,
+                Pbkdf2PasswordEncoder.SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256
+        );
+        Map<String, PasswordEncoder> encoders = new HashMap<>();
+        encoders.put("pbkdf2", pbkdf2Encoder);
+        DelegatingPasswordEncoder passwordEncoder = new DelegatingPasswordEncoder("pbkdf2", encoders);
+        passwordEncoder.setDefaultPasswordEncoderForMatches(pbkdf2Encoder);
+        return passwordEncoder;
+    }
+
+}
