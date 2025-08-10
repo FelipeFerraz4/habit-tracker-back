1bee81837a8226921df02f9dbd1378189eb40c26
FelipeFerraz4 <felipeferraz2402@gmail.com>
2025-08-10 17:41:51 -0300

    feat: create function exists user in user repository

--- src/main/java/space/algoritmos/habit_tracker_back/repository/UserRepository.java
+++ src/main/java/space/algoritmos/habit_tracker_back/repository/UserRepository.java
@@ -1,8 +1,6 @@
 package space.algoritmos.habit_tracker_back.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import space.algoritmos.habit_tracker_back.model.auth.User;
 
@@ -11,6 +9,6 @@ import java.util.UUID;
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
 
-    @Query("SELECT u FROM User u WHERE u.username =:username")
-    User findByUsername(@Param("username") String username);
+    User findByUsername(String username);
+    boolean existsByUsername(String username);
 }
